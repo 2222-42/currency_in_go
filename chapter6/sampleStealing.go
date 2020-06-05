c57d8f4190e3f26f15798eb3c5f9a2a8c2b0138b
2222-42 <stephen0617@me.com>
2020-06-05 21:01:12 +0900

    feat(chapter6): add flow of 6.1.1

--- chapter6/sampleStealing.go
+++ chapter6/sampleStealing.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func fib(n int) <-chan int {
 	result := make(chan int)
-	go func() {
+	go func() { // Goではゴルーチンはタスク
 		defer close(result)
 		if n <= 2 {
 			result <- 1
@@ -12,7 +12,7 @@ func fib(n int) <-chan int {
 		}
 		result <- <-fib(n-1) + <-fib(n-2)
 	}()
-	return result
+	return result // ゴルーチンのあとのものは全て継続と呼ばれる
 }
 
 func main() {
