2238d53199a466734a4ebbd273d48a62c0782607
2222-42 <stephen0617@me.com>
2020-06-05 09:49:38 +0900

    feat(chapter6): finish 6.1.0

---
+++ chapter6/sampleStealing.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func fib(n int) <-chan int {
+	result := make(chan int)
+	go func() {
+		defer close(result)
+		if n <= 2 {
+			result <- 1
+			return
+		}
+		result <- <-fib(n-1) + <-fib(n-2)
+	}()
+	return result
+}
+
+func main() {
+	fmt.Printf("result: %v", <-fib(10))
+}
