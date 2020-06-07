3d3f1467da668550b3d4f626a3f69fb1f84b242d
2222-42 <stephen0617@me.com>
2020-06-08 08:13:22 +0900

    chore(chapter Appendix A): finish A.3

---
+++ chapterApdxA/sampleProfile.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"runtime/pprof"
+	"time"
+)
+
+func main() {
+	log.SetFlags(log.Ltime | log.LUTC)
+	log.SetOutput(os.Stdout)
+
+	go func() {
+		goroutines := pprof.Lookup("goroutine")
+		for range time.Tick(1 * time.Second) {
+			log.Printf("goroutine count: %d\n", goroutines.Count())
+		}
+	}()
+
+	var blockForever chan struct{}
+	for i := 0; i < 10; i++ {
+		go func() { <-blockForever }()
+		time.Sleep(500 * time.Millisecond)
+	}
+}
